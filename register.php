521a17bd1b152cce59d990df40788e586dd993c2
Daniel <callejasmark63@gmail.com>
2025-05-07 21:29:41 +0800

    ❄️ Sign Up light mode

--- register.php
+++ register.php
@@ -3,6 +3,8 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
+    $view_mode = isset($_COOKIE['view']) ? $_COOKIE['view'] : 'light';
+
     include 'includes/db_connect.php';
 
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
@@ -17,7 +19,7 @@
 
         // Check if passwords match
         if ($password !== $confirm) {
-            echo "<div id='toast-data' data-message=' ❌ Passwords do not match' data-type='error'></div>";
+            echo "<div id='toast-data' data-message=' ❌ Password do not match' data-type='error' data-img='/phpets/hell_nah.png'></div>";
         } else {
             // Check if email already exists
             $check_sql = "SELECT user_id FROM users WHERE email = ?";
@@ -58,93 +60,112 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/index.css" />
-    <link rel="stylesheet" href="./assets/css/signup.css" />
-    <link rel="icon" type="image/svg" href="./assets/images/paw.svg" />
-    <title>Sign Up to PHPets!</title>
-</head>
-<body>
-    <div class="signup-page">
-        <div class="signup-main">
-            <div class="left">
-                <div class="text-content">
-                    <div class="logo">
-                        <img src="./assets/images/paw.svg" />
-                        <span class="violet">PHP</span><span class="white">ets</span>
-                    </div>
-                    <div class="middle">
-                        <p>High-quality supplies tailored for your pet’s health, happiness, and style—delivered to your door.</p>
-                        <div class="view-products-wrapper">
-                            <a class="view-products" href="index.php">
-                                <span >View Products</span>
-                            </a>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php if ($view_mode == 'dark'): ?>
+            <link rel="stylesheet" href="./assets/css/index.css" />
+            <link rel="stylesheet" href="./assets/css/signup.css" />
+        <?php else: ?>
+            <link rel="stylesheet" href="./assets/css/index-light.css" />
+            <link rel="stylesheet" href="./assets/css/signup-light.css" />
+        <?php endif ?>
+        <link rel="icon" type="image/svg" href="./assets/images/paw.svg" />
+        <title>Sign Up to PHPets!</title>
+    </head>
+    <body>
+        <div class="signup-page">
+            <div class="signup-main">
+                <div class="left">
+                    <div class="text-content">
+                        <div class="logo">
+                            <img src="./assets/images/paw.svg" />
+                            <span class="violet">PHP</span><span class="white">ets</span>
                         </div>
+                        <div class="middle">
+                            <p>High-quality supplies tailored for your pet’s health, happiness, and style—delivered to your door.</p>
+                            <div class="view-products-wrapper">
+                                <a class="view-products" href="index.php">
+                                    <span >View Products</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div></div>
+                        
                     </div>
-                    <div></div>
                     
                 </div>
-                
-            </div>
-            <div class="right">
-                <form class="signup" action="register.php" method="POST">
-                    <h1>Sign Up</h1>
-                    <div class="page-indicator">
-                        <img src="./assets/images/one-closed.svg" class="one">
-                        <img src="./assets/images/check-circle-svgrepo-com.svg" class="checkmark hidden" alt="">
-                        <span>---------</span>
-                        <img src="./assets/images/two.svg" alt="">
-                    </div>
-                    <div class="signin-part-1">
-                        <label for="first_name">First Name:</label>
-                        <input type="text" id="first_name" name="first_name" placeholder="Enter your first name" required />
+                <div class="right">
+                    <form class="signup" action="register.php" method="POST">
+                        <h1>Sign Up</h1>
+                        <div class="page-indicator">
+                            <?php if ($view_mode == 'dark'): ?>
+                                <img src="./assets/images/one-closed.svg" class="one">
+                                <img src="./assets/images/check-circle-svgrepo-com.svg" class="checkmark hidden" alt="">
+                            <?php else: ?>
+                                <img src="./assets/images/one-dark.svg" class="one">
+                                <img src="./assets/images/check-dark.svg" class="checkmark hidden" alt="">
+                            <?php endif ?>
+                            <span>---------</span>
+                            <?php if ($view_mode == 'dark'): ?>
+                                <img src="./assets/images/two.svg" alt="">
+                            <?php else: ?>
+                                <img src="./assets/images/two-dark.svg" alt="">
+                            <?php endif ?>
+                        </div>
+                        <div class="signin-part-1">
+                            <label for="first_name">First Name:</label>
+                            <input type="text" id="first_name" name="first_name" placeholder="Enter your first name" required />
 
-                        <label for="middle_name">Middle Name:</label>
-                        <input type="text" id="middle_name" name="middle_name" placeholder="Enter your middle name (optional)" />
+                            <label for="middle_name">Middle Name:</label>
+                            <input type="text" id="middle_name" name="middle_name" placeholder="Enter your middle name (optional)" />
 
-                        <label for="last_name">Last Name:</label>
-                        <input type="text" id="last_name" name="last_name" placeholder="Enter your last name" />
+                            <label for="last_name">Last Name:</label>
+                            <input type="text" id="last_name" name="last_name" placeholder="Enter your last name" />
 
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" placeholder="Enter your email" required />
-                    </div>
-                    <div class="signin-part-2 hidden">
-                        <label for="password">Password:</label>
-                        <input type="password" id="password" name="password" placeholder="Enter your password" required />
+                            <label for="email">Email:</label>
+                            <input type="email" id="email" name="email" placeholder="Enter your email" required />
+                        </div>
+                        <div class="signin-part-2 hidden">
+                            <label for="password">Password:</label>
+                            <input type="password" id="password" name="password" placeholder="Enter your password" required />
 
-                        <label for="confirm_password">Confirm Password:</label>
-                        <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm your password" required />
+                            <label for="confirm_password">Confirm Password:</label>
+                            <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm your password" required />
 
-                        <label for="address">Address:</label>
-                        <input type="tex" name="address" placeholder="Street, Barangay, Municipal, Province">
+                            <label for="address">Address:</label>
+                            <input type="tex" name="address" placeholder="Street, Barangay, Municipal, Province">
 
-                        <div class="radio-group">
-                            <div class="choice selected">
-                                <input type="radio" name="account_type" value="buyer" checked required />
-                                <span> Buyer</span>
-                                <span></span>
-                            </div>
-                            <div class="choice ">
-                                <input type="radio" name="account_type" value="seller" required />
-                                <span> Seller</span>
-                                <span></span>
+                            <div class="radio-group">
+                                <div class="choice selected">
+                                    <input type="radio" name="account_type" value="buyer" checked required />
+                                    <span> Buyer</span>
+                                    <span></span>
+                                </div>
+                                <div class="choice ">
+                                    <input type="radio" name="account_type" value="seller" required />
+                                    <span> Seller</span>
+                                    <span></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <button type="submit" class="signup-btn hidden">Sign Up</button>
-                    <button class="next-back">Next</button>
-                    <a class="white-btn" href="login.php" style="width: 100%;">
-                        Already Have an Account? Log In
-                    </a>
-                </form>
+                        <button type="submit" class="signup-btn hidden">Sign Up</button>
+                        <button class="next-back">Next</button>
+                        <a class="white-btn" href="login.php" style="width: 100%;">
+                            Already Have an Account? Log In
+                        </a>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-    <div id="toast-container"></div>
-    <script src="./assets/js/toast.js"></script>
-    <script src="./assets/js/register.js"></script>
-</body>
+        <div id="toast-container"></div>
+        <script src="./assets/js/toast.js"></script>
+        <script src="./assets/js/register.js"></script>
+    </body>
 </html>
+
+<?php 
+    include 'includes/view-modal.php';
+
+?>
