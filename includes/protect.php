76ba5ccce6f2741e852eaed261c6762846782a4a
Daniel <callejasmark63@gmail.com>
2025-05-25 21:21:08 +0800

    ❄️ protect.php bug fixed. Removed session start. Check if session user exist before logic

--- includes/protect.php
+++ includes/protect.php
@@ -1,21 +1,24 @@
 <?php
-    session_start();
     include 'db_connect.php';
+    include './error_catch.php';
 
-    $user_id = $_SESSION['user_id'];
-    $status_query = "SELECT status FROM users WHERE user_id = ?";
-    $stmt = $conn->prepare($status_query);
-    $stmt->bind_param("i", $user_id);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $user = $result->fetch_assoc();
+    if (isset($_SESSION['user_id'])) {
 
-    // Check if the user's status is 'banned'
-    if ($user && $user['status'] === 'banned') {
-        // Log the user out
-        session_unset();
-        session_destroy();
-        header("Location: /phpets/includes/banned.php");
-        exit();
+        $user_id = $_SESSION['user_id'];
+        $status_query = "SELECT status FROM users WHERE user_id = ?";
+        $stmt = $conn->prepare($status_query);
+        $stmt->bind_param("i", $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
+
+        if ($user && $user['status'] === 'banned') {
+            // Log the user out
+            session_unset();
+            session_destroy();
+            header("Location: /phpets/includes/banned.php");
+            exit();
+        }
     }
+    
 ?>
